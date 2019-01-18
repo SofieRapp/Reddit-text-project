7847e79f83e144e9b012bdd0378538fff5ce939d
Sofie Rapp <rapsof15@student.westerdals.no>
2019-01-18 15:37:07 +0100

    Vader sentiment on league posts

--- src/main/python/vader_sentiment.py
+++ src/main/python/vader_sentiment.py
@@ -1,9 +1,28 @@
+import pandas as pd
 from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
 
-analyser = SentimentIntensityAnalyzer()
+
+analyzer = SentimentIntensityAnalyzer()
+
+textlines = [line.rstrip('\n')
+             for line in open('..\\resources\\leagueoflegends.txt', encoding='utf-8')]
+
+title = []
+compound = []
+positive = []
+negative = []
+
+for i in range(0, len(textlines)):
+    scores = analyzer.polarity_scores(textlines[i])
+    # Removes post that doesn't have a sentiment
+    if scores['compound'] != 0:
+        title.append(textlines[i])
+        compound.append(scores['compound'])
+        positive.append(scores['pos'])
+        negative.append(scores['neg'])
 
 
-def print_sentiment_scores(sentence):
-    snt = analyser.polarity_scores(sentence)
-    print("{:-<40} {}".format(sentence, str(snt)))
+leauge_df = pd.DataFrame({'Title' : title, 'positive' : positive, 'negative' : negative, 'compound' : compound})
 
+
+print(leauge_df)
