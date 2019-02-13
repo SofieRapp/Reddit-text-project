409d2321911e03fb15e4f970588e8d27589957a6
Sofie Rapp <rapsof15@student.westerdals.no>
2019-02-13 19:49:48 +0100

    small changes

---
+++ README.md
@@ -0,0 +1,6 @@
+WIP
+Current problems with the sentiment analysis:
+Team names being labled as negative or positive
+- Quickfix, remove post match discussion post since we know these titles have no sentiment.
+Doesn't filter well on gaming slang, ex. insane outplay gets a negative sentiment score when this is considered something good
+- No quick fix, some mislabeling is fine can still use the scores when categorized
--- src/main/python/vader_sentiment.py
+++ src/main/python/vader_sentiment.py
@@ -4,25 +4,31 @@ from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
 
 analyzer = SentimentIntensityAnalyzer()
 
-textlines = [line.rstrip('\n')
-             for line in open('..\\resources\\leagueoflegends.txt', encoding='utf-8')]
-
-title = []
-compound = []
-positive = []
-negative = []
-
-for i in range(0, len(textlines)):
-    scores = analyzer.polarity_scores(textlines[i])
-    # Removes post that doesn't have a sentiment
-    if scores['compound'] != 0:
-        title.append(textlines[i])
-        compound.append(scores['compound'])
-        positive.append(scores['pos'])
-        negative.append(scores['neg'])
+text_lol = [line.rstrip('\n') for line in open('..\\resources\\leagueoflegends.txt', encoding='utf-8')]
+text_ow = [line.rstrip('\n') for line in open('..\\resources\\overwatch.txt', encoding='utf-8')]
 
 
-leauge_df = pd.DataFrame({'Title' : title, 'positive' : positive, 'negative' : negative, 'compound' : compound})
+def create_scores(text_lines):
+    title = []
+    compound = []
+    positive = []
+    negative = []
+
+    for i in range(0, len(text_lines)):
+        if 'Post-Match Discussion' in text_lines[i]:
+            continue
+        scores = analyzer.polarity_scores(text_lines[i])
+        # Removes post that doesn't have a sentiment
+        if scores['compound'] != 0:
+            title.append(text_lines[i])
+            compound.append(scores['compound'])
+            positive.append(scores['pos'])
+            negative.append(scores['neg'])
+
+    return pd.DataFrame({'Title' : title, 'positive' : positive, 'negative' : negative, 'compound' : compound})
 
 
-print(leauge_df)
+league_df = create_scores(text_lol)
+ow_df = create_scores(text_ow)
+
+print(league_df)
